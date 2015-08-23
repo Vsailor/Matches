2f3ba418899eb2194696c7f877dffb37ac06c5c1
Ivan <sirius9764@gmail.com>
2015-08-23 19:40:31 +0300

    Added background and resolutions

---
+++ Matches/Assets/Scripts/ResolutionScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResolutionScript : MonoBehaviour
+{
+    public Transform MainBackgroundTransform;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+    void OnGUI()
+    {
+        var resolution = string.Empty;
+        float height = 0.5280859f;
+        try
+        {
+            resolution = (Screen.width * 1.0 / Screen.height * 1.0).ToString().Substring(0, 4);
+        }
+        catch (System.ArgumentOutOfRangeException)
+        {
+            resolution = (Screen.width * 1.0 / Screen.height * 1.0).ToString();
+        }
+        MainBackgroundTransform.localScale = new Vector3(float.Parse(resolution) / 1.05f, height, 1f);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
