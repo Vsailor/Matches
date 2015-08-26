66c33b88a2d451b91accce2d7f347bad953de6a5
Ivan <sirius9764@gmail.com>
2015-08-26 23:10:24 +0300

    Done GUI

--- Matches/Assets/Scripts/BoxesScript.cs
+++ Matches/Assets/Scripts/BoxesScript.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 
 public class BoxesScript : MonoBehaviour {
+    public Texture BoxTexture;
 	// Use this for initialization
 	void Start () {
         int count = 1;
@@ -9,7 +10,9 @@ public class BoxesScript : MonoBehaviour {
         {
             for (int j = 1; j <= 21 && count<=100; j++)
             {
-                transform.FindChild("Boxes (" + i + ")").FindChild("Box (" +  j + ")").FindChild("Text").GetComponent<UnityEngine.UI.Text>().text = count.ToString();
+                var box = transform.FindChild("Boxes (" + i + ")").FindChild("Box (" + j + ")");
+                box.FindChild("Text").GetComponent<UnityEngine.UI.Text>().text = count.ToString();
+                box.GetComponent<UnityEngine.UI.RawImage>().texture = BoxTexture;
                 count++;
             }
         }
