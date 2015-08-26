e33e7d059857ed9471ed2893abfc3264970b95c1
Ivan <sirius9764@gmail.com>
2015-08-26 18:15:55 +0300

    Modified buttons and adds scripts for each button

---
+++ Matches/Assets/Scripts/RestoreScript.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RestoreScript : MonoBehaviour {
+    public void Restore()
+    {
+
+    }
+}
---
+++ Matches/Assets/Scripts/WriteAReviewScript.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WriteAReviewScript : MonoBehaviour {
+    public void WriteAReview()
+    {
+        
+    }
+}
