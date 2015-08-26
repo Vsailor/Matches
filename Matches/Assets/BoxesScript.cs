f984053353da3b6539d7745719776e86f9d382d9
Ivan <sirius9764@gmail.com>
2015-08-26 13:18:54 +0300

    Scroll view on levels menu done

---
+++ Matches/Assets/BoxesScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoxesScript : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+        int count = 1;
+        for (int i = 1; i <= 5; i++)
+        {
+            for (int j = 1; j <= 21 && count<=100; j++)
+            {
+                transform.FindChild("Boxes (" + i + ")").FindChild("Box (" +  j + ")").FindChild("Text").GetComponent<UnityEngine.UI.Text>().text = count.ToString();
+                count++;
+            }
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+}
