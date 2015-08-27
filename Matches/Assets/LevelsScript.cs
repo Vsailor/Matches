a0146e1ba4369553fe240a249c5a229018f61a02
Ivan <sirius9764@gmail.com>
2015-08-27 18:57:35 +0300

    Logic almost done

--- Matches/Assets/LevelsScript.cs
+++ Matches/Assets/LevelsScript.cs
@@ -2,20 +2,57 @@
 using System.Collections;
 
 public class LevelsScript : MonoBehaviour {
-    public GameObject[] StartMatches;
-	void Start () {
-        // Clear UI matches
+    public int MaxTakenMatchesCount;
+    public int[] StartMatches;
+    public System.Collections.Generic.List<int> CurrentPositions;
+    int takenMatchesCount;
+    public int TakenMatchesCount
+    {
+        get
+        {
+            return takenMatchesCount;
+        }
+        set
+        {
+            takenMatchesCount = value;
+            SetActiveUIMatches(value);
+        }
+    }
+    void Start () {
+        TakenMatchesCount = 0;
+        CurrentPositions = new System.Collections.Generic.List<int>();
         SetActiveUIMatches(0);
-        // Set color matches
-        var sprite = Resources.Load<Sprite>("Matches_1");
         foreach (var item in StartMatches)
         {
-            item.SendMessage("SetActive", true);
+            transform.FindChild("Matches").FindChild("Match (" + item + ")").GetComponent<MatchScript>().SetActive(true);
         }
-        var matches = transform.FindChild("Matches");
 
 	}
-    void SetActiveUIMatches(int count)
+    public bool MovementDone()
+    {
+        var currCopy = CurrentPositions.GetRange(0, CurrentPositions.Count);
+        var startMatchesCopy = new System.Collections.Generic.List<int>();
+        foreach (var item in StartMatches)
+        {
+            startMatchesCopy.Add(item);
+        }
+        currCopy.Sort();
+        startMatchesCopy.Sort();
+        int counter = 0;
+        for (int i = 0; i < currCopy.Count; i++)
+        {
+            if (currCopy[i] != startMatchesCopy[i])
+            {
+                counter++;
+            }
+        }
+        if (counter / 2 == MaxTakenMatchesCount)
+        {
+            return true;
+        }
+        return false;
+    }
+    public void SetActiveUIMatches(int count)
     {
         var uiMatches = transform.FindChild("UIMatches");
         for (int i = count+1; i <= 5; i++)
--- Matches/Assets/MatchScript.cs
+++ Matches/Assets/MatchScript.cs
@@ -3,19 +3,42 @@ using System.Collections;
 
 public class MatchScript : MonoBehaviour
 {
+    public int Number;
     public bool IsActive;
     Sprite DisabledSprite;
     Sprite EnabledSprite;
     void OnMouseDown()
     {
-        SetActive(!IsActive);
+        if (IsActive)
+        {
+            if (transform.parent.parent.GetComponent<LevelsScript>().MaxTakenMatchesCount >
+                transform.parent.parent.GetComponent<LevelsScript>().TakenMatchesCount)
+            {
+                transform.parent.parent.GetComponent<LevelsScript>().CurrentPositions.Remove(Number);
+                transform.parent.parent.GetComponent<LevelsScript>().TakenMatchesCount++;
+                SetActive(false);
+           }
+        }
+        else
+        {
+            if (transform.parent.parent.GetComponent<LevelsScript>().TakenMatchesCount > 0)
+            {
+                transform.parent.parent.GetComponent<LevelsScript>().TakenMatchesCount--;
+                transform.parent.parent.GetComponent<LevelsScript>().CurrentPositions.Add(Number);
+                SetActive(true);
+            }
+        }
+
     }
     void Start()
     {
+        var s = name.Remove(0, name.IndexOf('(')+1);
+        Number = int.Parse(s.Remove(s.IndexOf(')')));
+        
         EnabledSprite = Resources.Load<Sprite>("Matches_1");
         DisabledSprite = Resources.Load<Sprite>("Matches_2");
     }
-    void SetActive(bool active)
+    public void SetActive(bool active)
     {
         IsActive = active;
         if (active)
