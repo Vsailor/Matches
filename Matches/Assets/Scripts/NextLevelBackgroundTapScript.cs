87bdb5a84a93a6f841fcc7a6ecc3cb746734659c
Ivan <sirius9764@gmail.com>
2015-09-04 17:38:40 +0300

    Fixed bugs

--- Matches/Assets/Scripts/NextLevelBackgroundTapScript.cs
+++ Matches/Assets/Scripts/NextLevelBackgroundTapScript.cs
@@ -5,7 +5,10 @@ public class NextLevelBackgroundTapScript : MonoBehaviour {
     public void OnMouseDown()
     {
         var levelNuber = int.Parse(transform.parent.name.Remove(0, 5));
-        PlayerPrefs.SetInt("Levels", levelNuber+1);
+        if (PlayerPrefs.GetInt("Levels") == levelNuber)
+        {
+            PlayerPrefs.SetInt("Levels", levelNuber + 1);
+        }
         transform.parent.GetComponent<LevelsScript>().NextLevel.SetActive(true);
         transform.parent.transform.FindChild("Task").GetComponent<UnityEngine.UI.Text>().text = transform.parent.GetComponent<LevelsScript>().Task;
         this.GetComponent<BoxCollider2D>().enabled = false;
